0c4c5d2e4ad1a0e2ab9a20c6bf2cb7b5e0ed940a
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-09-29 21:57:59 -0700

    manually created SessionOptions instead of extending from express-session

--- types/FunnelFlo.d.ts
+++ types/FunnelFlo.d.ts
@@ -1,8 +1,16 @@
 import session from "express-session";
 
-interface SessionOptions extends session.SessionOptions {
+interface SessionOptions {
+  secret: string | string[];
+  genid?: (req: express.Request) => string;
+  name?: string;
+  store?: Store;
+  cookie?: CookieOptions;
+  rolling?: boolean;
   resave: boolean;
+  proxy?: boolean;
   saveUninitialized: boolean;
+  unset: "destroy" | "keep";
 }
 
 export interface FunnelFloOptions {
